912413d47b8812f942cf313f51756152939a854d
Gillan0 <antonino.gillard@orange.fr>
2025-04-03 01:39:05 +0200

    Attempt at Q°10.3

---
+++ expr/fun/toPfx.ml
@@ -0,0 +1,34 @@
+open Ast
+open FunPfx.Ast
+
+let p = Hashtbl.create 1;;
+
+let rec generate expr =  
+  let aux depth = function
+  
+  | Const v ->  [PUSH(v)]
+
+  | Binop(Badd,v1,v2) -> (aux depth v2)@(aux depth v1)@[ADD]
+  
+  | Binop(Bsub,v1,v2) ->(aux depth v2)@(aux depth v1)@[SUB]
+  
+  | Binop(Bmul,v1,v2) -> (aux depth v2)@(aux depth v1)@[MUL]
+  
+  | Binop(Bdiv,v1,v2) -> (aux depth v2)@(aux depth v1)@[DIV]
+  
+  | Binop(Bmod,v1,v2) -> (aux depth v2)@(aux depth v1)@[REM]
+  
+  | Uminus v -> (aux depth v)@[PUSH(0); SUB]
+  
+  | Var var_name -> (try 
+                      [GET (Hashtbl.find p var_name)]
+                    with Not_found -> failwith ("Unbound variable: " ^ var_name))
+  
+  | App(Fun(var_name, v2), v3) -> (aux depth v3)@(aux depth Fun(var_name, v2))@[EXEC]
+  
+  | App(_, _) -> failwith "Left term of application must be a function"
+  
+  | Fun(var_name, v) -> Hashtbl.add p var_name (depth+1); 
+                        [EXEC_SEQ (aux (depth+1) v)]
+  
+  in aux 0 expr;;
