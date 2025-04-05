f3f2fa6015e02295ec2563084471adfab2cccb75
Gillan0 <antonino.gillard@orange.fr>
2025-04-06 00:28:35 +0200

    Fix generate for Q°10

--- expr/fun/toPfx.ml
+++ expr/fun/toPfx.ml
@@ -7,52 +7,69 @@ let increment_all p =
     Hashtbl.replace p key (value + 1)
   ) p ;;
 
+  increment_all (Hashtbl.create 1)
+
+let decrement_all p =
+
+  Hashtbl.iter (fun key value ->
+    Hashtbl.replace p key (value - 1)
+  ) p ;;
+
+  decrement_all (Hashtbl.create 1)
+
 let generate expr =  
 
   let p = Hashtbl.create 1 in
 
-  let rec aux depth i = function
+  let rec aux p = function
   
-  | Const v -> increment_all p ; [PUSH(v)]
+  | Const v -> increment_all p; [PUSH(v)]
 
-  | Binop(Badd,v1,v2) -> let prio_term = aux depth (i+1) v2 in
-                        let last_term = aux depth (i+1) v1 in 
+  | Binop(Badd,v1,v2) -> let prio_term = aux p v2 in
+                        let last_term = aux p v1 in 
+                        let _ = decrement_all p in
                         (prio_term)@(last_term)@[ADD]
 
-  | Binop(Bsub,v1,v2) -> let prio_term = aux depth (i+1) v2 in
-                        let last_term = aux depth (i+1) v1 in 
+  | Binop(Bsub,v1,v2) -> let prio_term = aux p v2 in
+                        let last_term = aux p v1 in 
+                        let _ = decrement_all p in
                         (prio_term)@(last_term)@[SUB]
                         
-  | Binop(Bmul,v1,v2) -> let prio_term = aux depth (i+1) v2 in
-                        let last_term = aux depth (i+1) v1 in 
+  | Binop(Bmul,v1,v2) -> let prio_term = aux p v2 in
+                        let last_term = aux p v1 in 
+                        let _ = decrement_all p in
                         (prio_term)@(last_term)@[MUL]
 
-  | Binop(Bdiv,v1,v2) -> let prio_term = aux depth (i+1) v2 in
-                        let last_term = aux depth (i+1) v1 in 
+  | Binop(Bdiv,v1,v2) -> let prio_term = aux p v2 in
+                        let last_term = aux p v1 in 
+                        let _ = decrement_all p in
                         (prio_term)@(last_term)@[DIV]
           
-  | Binop(Bmod,v1,v2) -> let prio_term = aux depth (i+1) v2 in
-                        let last_term = aux depth (i+1) v1 in 
+  | Binop(Bmod,v1,v2) -> let prio_term = aux p v2 in
+                        let last_term = aux p v1 in 
+                        let _ = decrement_all p in
                         (prio_term)@(last_term)@[REM]
   
                         
-  | Uminus v -> (aux depth (i+1) v)@[PUSH(0); SUB]
+  | Uminus v -> (aux p v)@[PUSH(0); SUB]
   
   | Var var_name -> (try 
-                      increment_all p;
-                      let n = Hashtbl.find p var_name in 
-                      Hashtbl.replace p var_name (n-1);
-                      [PUSH (Hashtbl.find p var_name); GET]
+                      let v = Hashtbl.find p var_name in 
+                      let _ = increment_all p in
+                      [PUSH(v); GET]
                     with Not_found -> failwith ("Unbound variable: " ^ var_name))
   
-  | App(Fun(var_name, v2), v3) -> let prio_term = aux (depth + 1) (i+1) v3 in
-                                  let last_term = aux (depth) (i+1) (Fun(var_name, v2)) in 
-                                  prio_term@last_term@[EXEC; SWAP; POP] 
-                                  
+  | App(Fun(var_name, body), arg_expr) ->
+                      let arg_code = aux p arg_expr in
+              
+                      Hashtbl.replace p var_name 0;
+              
+                      let body_code = aux p (Fun(var_name, body)) in
+              
+                      arg_code @ body_code @ [EXEC; SWAP; POP]                                  
   
   | App(_, _) -> failwith "Left term of application must be a function"
   
-  | Fun(var_name, v) -> Hashtbl.add p var_name (depth); 
-                        [EXEC_SEQ(aux (depth) (i+1) v)]
+  | Fun(_, v) -> [EXEC_SEQ(aux p v)]
   
-  in aux 0 0 expr;;
+  in aux p expr;;
